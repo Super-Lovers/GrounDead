b0e5b5db116d685635420c2f485601b449bf2ae1
Super-Lovers <niki.ivanov98@gmail.com>
2018-09-12 19:57:40 +0300

    Added the hostile's territory and fixed entrances

--- SurvivalConcept/Program.cs
+++ SurvivalConcept/Program.cs
@@ -52,21 +52,45 @@ namespace SurvivalConcept
                     {
                         if (x % 13 == 0 && x != 0)
                         {
-                            gameWorld[y, x] = 7;
+                            if (y == mapHeight / 2 - 1)
+                            {
+                                gameWorld[y, x] = 6; // path going up
+                            }
+                            else
+                            {
+                                gameWorld[y, x] = 7; // path going down
+                            }
                         }
                         else
                         {
-                            gameWorld[y, x] = 9;
+                            gameWorld[y, x] = 9; // boundaries of biomes
                         }
                     } else if ((x < 63) && (y == mapHeight / 2))
                     {
-                        gameWorld[y, x] = 2;
+                        gameWorld[y, x] = 2; // path / road
                     } else if ((x == 75) && (y == mapHeight / 2))
                     {
-                        gameWorld[y, x] = 3;
+                        gameWorld[y, x] = 3; // house of the player
                     } else if (x % 20 == 0 && x != 0 && x != 2 && x < 61 && x != 7)
                     {
-                        gameWorld[y, x] = 9;
+                        gameWorld[y, x] = 9; // boundaries of biomes
+                    }
+                    
+                    // Drawing the path head and tail
+                    if ((x == 0 && y == mapHeight / 2) || (x == 62 && y == mapHeight / 2))
+                    {
+                        gameWorld[y, x] = 1; // path endings can be grass for simplicity
+                    }
+                    
+                    // Drawing the zombie's side of the game world
+                    if (x > 79)
+                    {
+                        gameWorld[y, x] = 8;
+                    }
+
+                    if (x > 60 && y > 13 && y < 27 && gameWorld[y, x] != 2 && gameWorld[y, x] != 1 && gameWorld[y, x] != 3)
+                    {
+                        gameWorld[y, x] = 8;
                     }
                     Console.Write(gameWorld[y, x]);
                 }
